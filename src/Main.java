517d2fe16e633e8614d414d01cd5b8d6be53674a
biskall <81960463+biskall@users.noreply.github.com>
2021-11-29 12:05:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         double[] myArray = new double[]{1,2,3,4.5 };
         System.out.println(Algorithms.zScore(1, myArray));
+        ReadCSV anomaly = new ReadCSV("anomaly_flight.csv");// test
     }
 }
